d64ba51b5609c2f829f2a32130d8f231dbfad615
Vansh Patel <93229167+Vansh-Patell@users.noreply.github.com>
2023-10-24 18:35:37 -0500

    Create GetAudio.py

---
+++ GetAudio.py
@@ -0,0 +1,19 @@
+import pyaudio
+
+# Create an instance of the PyAudio class
+p = pyaudio.PyAudio()
+
+# Open a streaming stream
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+
+print("Recording Audio...")
+
+# Start capturing audio
+while True:
+    data = stream.read(1024)  # Adjust the buffer size as needed
+    # Process or analyze the audio data here
+
+# Close the stream and PyAudio
+stream.stop_stream()
+stream.close()
+p.terminate()
